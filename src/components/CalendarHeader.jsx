f34f5620faa0e6f7d357b5f54ac74290eb696538
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-07-20 12:39:48 +0530

    context issue solved

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,6 +1,7 @@
 import { CalendarOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons'
 import React, { useContext } from 'react'
 import Context from '../context/Context'
+import dayjs from 'dayjs';
 
 const CalendarHeader = () => {
     const {monthIdx,setMonthIdx} = useContext(Context);
@@ -11,24 +12,35 @@ const CalendarHeader = () => {
     const handleNextMonth = ()=>{
         setMonthIdx(monthIdx+1);
     }
+
+    const handleToday = () =>{
+        setMonthIdx(dayjs().month());
+    }
+
   return (
     <header className='px-4 py-2 flex items-center'>
         <CalendarOutlined style={{fontSize: '20px', color: '#38f'}} className='mr-2 border border-gray-200 p-2 rounded-full' />
         <h1 className='mr-10 text-xl text-gray-500 font-bold'>Calendar</h1>
 
-        <button className="border border-gray-200 rounded py-2 px-4 mr-5">
+        <button
+        onClick={handleToday}
+         className="border border-gray-200 rounded py-2 px-4 mr-5 cursor-pointer">
             Today
         </button>
-         <button onClick={handlePrevMonth()}>
+         <button onClick={handlePrevMonth}>
             <span className='cursor-pointer text-gray-500 mx-2'>
                 <LeftOutlined />
             </span>
         </button>
-        <button onClick={handleNextMonth()}>
+        <button onClick={handleNextMonth}>
             <span className='cursor-pointer text-gray-500 mx-2'>
                 <RightOutlined />
             </span>
         </button>
+
+        <h2 className='ml-4 text-xl text-gray-500 font-bold'>
+            {dayjs(new Date(dayjs().year(),monthIdx)).format("MMMM YYYY")}
+        </h2>
     </header>
   )
 }
--- src/context/ContextWrapper.jsx
+++ src/context/ContextWrapper.jsx
@@ -1,13 +1,12 @@
-import { ContainerTwoTone } from '@ant-design/icons'
 import React, { useState } from 'react'
 import Context from './Context'
 import dayjs from 'dayjs'
 
-const ContextWrapper = ({childern}) => {
+const ContextWrapper = ({children}) => {
     const  [monthIdx,setMonthIdx] = useState(dayjs().month())
   return (
     <Context.Provider value={{monthIdx,setMonthIdx}}>
-        {childern}
+        {children}
     </Context.Provider>
   )
 }
